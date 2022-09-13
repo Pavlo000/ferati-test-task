1563a06fd81f699917e3f74af0f610354c447194
Pavlo <Pmoroz2004@gmail.com>
2022-09-13 17:05:53 +0300

    v.2

--- src/features/NavList/NavList.tsx
+++ src/features/NavList/NavList.tsx
@@ -3,7 +3,6 @@ import text from './NavListTextContent.json';
 import './NavList.scss';
 import { NavLink } from 'react-router-dom';
 import { Icon } from '../Icon';
-import { Lang } from '../Lang';
 
 export const NavList: React.FC = () => {
   const lang = useAppSelector(state => state.lang.lang);
